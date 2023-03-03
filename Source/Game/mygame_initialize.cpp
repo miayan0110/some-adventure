8068a73cd9f1454a8c30135d2ea0eddeb84c1c23
miayan0110 <miayan0110@gmail.com>
2023-03-03 10:00:19 +0800

    v1.0

---
+++ Source/Game/mygame_initialize.cpp
@@ -0,0 +1,73 @@
+#include "stdafx.h"
+#include "../Core/Resource.h"
+#include <mmsystem.h>
+#include <ddraw.h>
+#include "../Library/audio.h"
+#include "../Library/gameutil.h"
+#include "../Library/gamecore.h"
+#include "mygame.h"
+
+using namespace game_framework;
+/////////////////////////////////////////////////////////////////////////////
+// 這個class為遊戲的遊戲開頭畫面物件
+/////////////////////////////////////////////////////////////////////////////
+
+CGameStateInit::CGameStateInit(CGame *g) : CGameState(g)
+{
+}
+
+void CGameStateInit::OnInit()
+{
+	//
+	// 當圖很多時，OnInit載入所有的圖要花很多時間。為避免玩遊戲的人
+	//     等的不耐煩，遊戲會出現「Loading ...」，顯示Loading的進度。
+	//
+	ShowInitProgress(0, "Start Initialize...");	// 一開始的loading進度為0%
+	Sleep(200);
+	
+	InitBackground();
+
+	ShowInitProgress(50, "Initialize...");
+	//
+	// 開始載入資料
+	//
+	// Sleep(1000);				// 放慢，以便看清楚進度，實際遊戲請刪除此Sleep
+	//
+	// 此OnInit動作會接到CGameStaterRun::OnInit()，所以進度還沒到100%
+	//
+}
+
+void CGameStateInit::OnBeginState()
+{
+}
+
+void CGameStateInit::OnKeyUp(UINT nChar, UINT nRepCnt, UINT nFlags)
+{
+	GotoGameState(GAME_STATE_RUN);		// 切換至GAME_STATE_RUN
+}
+
+void CGameStateInit::OnLButtonDown(UINT nFlags, CPoint point)
+{
+	
+}
+
+void CGameStateInit::OnShow()
+{
+	background.ShowBitmap();
+	InitText();
+}
+
+void CGameStateInit::InitBackground() {
+	background.LoadBitmap({ "resources/initialize_background.bmp" });
+	background.SetTopLeft(0, 0);
+}
+
+void CGameStateInit::InitText() {
+	CDC *pDC = CDDraw::GetBackCDC();
+	CFont* fp;
+
+	CTextDraw::ChangeFontLog(pDC, fp, 36, "微軟正黑體", RGB(255, 255, 255));
+	CTextDraw::Print(pDC, 150, 425,"Press any key to start");
+
+	CDDraw::ReleaseBackCDC();
+}
---
+++ Source/Game/mygame_run.cpp
@@ -0,0 +1,106 @@
+#include "stdafx.h"
+#include "../Core/Resource.h"
+#include <mmsystem.h>
+#include <ddraw.h>
+#include "../Library/audio.h"
+#include "../Library/gameutil.h"
+#include "../Library/gamecore.h"
+#include "mygame.h"
+
+using namespace game_framework;
+
+/////////////////////////////////////////////////////////////////////////////
+// 這個class為遊戲的遊戲執行物件，主要的遊戲程式都在這裡
+/////////////////////////////////////////////////////////////////////////////
+
+CGameStateRun::CGameStateRun(CGame *g) : CGameState(g)
+{
+}
+
+CGameStateRun::~CGameStateRun()
+{
+}
+
+void CGameStateRun::OnBeginState()
+{
+}
+
+void CGameStateRun::OnMove()							// 移動遊戲元素
+{
+	
+}
+
+void CGameStateRun::OnInit()  								// 遊戲的初值及圖形設定
+{
+	background.LoadBitmapByString({
+		"resources/initialize_background.bmp"
+	}, RGB(255, 255, 255));
+	background.SetTopLeft(0, 0);
+
+	character.LoadBitmapByString({
+//		"resources/giraffe.bmp"
+		"resources/pacman_towardR_1.bmp",
+//		"resources/pacman_towardR_2.bmp",
+	}, RGB(255, 255, 255));
+	character.SetTopLeft(150, 150);
+	//character.SetAnimation(500, 0);
+}
+
+void CGameStateRun::OnKeyDown(UINT nChar, UINT nRepCnt, UINT nFlags)
+{
+	//if (phase == 1) {
+	//	if (nChar == VK_RIGHT) {
+	//		character.ShowBitmap();
+	//	}
+	//}
+
+	if (nChar == 0x44) {
+		character.SetTopLeft(character.Left() + 50, character.Top());	// D for going right
+	}
+	else if (nChar == 0x41) {
+		character.SetTopLeft(character.Left() - 50, character.Top());	// A for going left
+	}
+	else if (nChar == 0x57) {
+		character.SetTopLeft(character.Left(), character.Top() - 50);	// W for going up
+	}
+	else if (nChar == 0x53) {
+		character.SetTopLeft(character.Left(), character.Top() + 50);	// S for going down
+	}
+}
+
+void CGameStateRun::OnKeyUp(UINT nChar, UINT nRepCnt, UINT nFlags)
+{
+	
+}
+
+void CGameStateRun::OnLButtonDown(UINT nFlags, CPoint point)  // 處理滑鼠的動作
+{
+}
+
+void CGameStateRun::OnLButtonUp(UINT nFlags, CPoint point)	// 處理滑鼠的動作
+{
+}
+
+void CGameStateRun::OnMouseMove(UINT nFlags, CPoint point)	// 處理滑鼠的動作
+{
+}
+
+void CGameStateRun::OnRButtonDown(UINT nFlags, CPoint point)  // 處理滑鼠的動作
+{
+}
+
+void CGameStateRun::OnRButtonUp(UINT nFlags, CPoint point)	// 處理滑鼠的動作
+{
+}
+
+void CGameStateRun::OnShow()
+{
+	ShowByPhase();
+}
+
+void CGameStateRun::ShowByPhase() {
+	background.ShowBitmap(5);
+	if (phase == 1) {
+		character.ShowBitmap(5);
+	}
+}
